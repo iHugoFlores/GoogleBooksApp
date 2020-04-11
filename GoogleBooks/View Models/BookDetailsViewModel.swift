48c0011f3f8bddaacc359d1e184734f69d62ecac
Hugo Flores <hfp.ing2@gmail.com>
2020-04-11 18:19:54 -0400

    Book details view and view model

--- GoogleBooks/View Models/BookDetailsViewModel.swift
+++ GoogleBooks/View Models/BookDetailsViewModel.swift
@@ -8,4 +8,7 @@
 import Foundation
 
 class BookDetailsViewModel {
+    var model: Volume?
+
+    var onModelSet: (() -> Void)?
 }
--- GoogleBooks/Views/BookDetailsController.swift
+++ GoogleBooks/Views/BookDetailsController.swift
@@ -8,9 +8,84 @@
 import UIKit
 
 class BookDetailsController: UIViewController {
+    static let segueIdentifier: String = "toBookDetails"
+    let viewModel = BookDetailsViewModel()
+
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+
+    private let mainContainer: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 32
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let bookImage: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.contentMode = .scaleAspectFit
+        return imgView
+    }()
+
+    private let titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textColor = .black
+        lbl.font = UIFont.boldSystemFont(ofSize: 16)
+        lbl.numberOfLines = 3
+        return lbl
+    }()
+
+    private let authorLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textColor = .black
+        lbl.font = UIFont.systemFont(ofSize: 20)
+        lbl.numberOfLines = 1
+        return lbl
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpScrollView()
+        setUpMain()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = viewModel.model?.volumeInfo.title
+    }
+
+    func setUpScrollView() {
+           view.addSubview(scrollView)
+           scrollView.addSubview(mainContainer)
+
+           NSLayoutConstraint.activate([
+               scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+               scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+               scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+               scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+               mainContainer.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+               mainContainer.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+               mainContainer.topAnchor.constraint(greaterThanOrEqualTo: scrollView.topAnchor, constant: 16),
+               mainContainer.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor, constant: -16.0),
+
+               mainContainer.widthAnchor.constraint(equalTo: view.widthAnchor)
+           ])
+       }
+
+    func setUpMain() {
+        mainContainer.addArrangedSubview(bookImage)
+        mainContainer.addArrangedSubview(titleLabel)
+        mainContainer.addArrangedSubview(authorLabel)
+
+        NSLayoutConstraint.activate([
+            bookImage.leftAnchor.constraint(equalTo: mainContainer.leftAnchor),
+            bookImage.rightAnchor.constraint(equalTo: mainContainer.rightAnchor),
+            bookImage.heightAnchor.constraint(equalToConstant: view.frame.height / 4)
+        ])
     }
 }
