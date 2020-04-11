960b39f073873ef7725fdaa0ad0b69c58b192dd6
Hugo Flores <hfp.ing2@gmail.com>
2020-04-11 11:24:59 -0400

    Book Table cell component

---
+++ GoogleBooks/Views/Cells/BooksTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  BooksTableCell.swift
+//  GoogleBooks
+//
+//  Created by Hugo Flores Perez on 4/11/20.
+//
+
+import UIKit
+
+class BooksTableCell: UITableViewCell {
+    static let identifier = "BookCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
