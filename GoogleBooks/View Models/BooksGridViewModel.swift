a7499bef69b5efcd66b7d87cb328e45d20cbb3b8
Hugo Flores <hfp.ing2@gmail.com>
2020-04-11 21:37:21 -0400

    Favorites toggle and favorites re-load

--- GoogleBooks/View Models/BooksGridViewModel.swift
+++ GoogleBooks/View Models/BooksGridViewModel.swift
@@ -8,33 +8,48 @@
 import UIKit
 
 class BooksGridViewModel: NSObject {
-    var lastPage = 0
-
     var volumes: [Volume] = [] {
         didSet {
-            guard let onDataReload = onDataReload else {
+            if oldValue.count == volumes.count && !fetchNewDataLock {
                 return
             }
+            guard let onDataReload = onDataReload else { return }
             onDataReload()
         }
     }
 
+    var navbarRightButtonText: String {
+        return showQueryBooks ? "Favorites" : "Search"
+    }
+
+    var showQueryBooks: Bool = true {
+        didSet {
+            guard let onViewModeChanged = onViewModeChanged else { return }
+            onViewModeChanged()
+        }
+    }
+
+    var lastPage = 0
+    var favorites = Set<Volume>()
     var onDataReload: (() -> Void)?
-
+    var onViewModeChanged: (() -> Void)?
+    var onSingleRowReload: ((IndexPath) -> Void)?
     var query = ""
-
     var nextPageFetchLock = true
+    var fetchNewDataLock = false
 
     func searchNewQuery() {
-        GoogleBooksAPI.getNextPageWithQuery(query: query, page: 0) { data, error  in
+        fetchNewDataLock = true
+        GoogleBooksAPI.getNextPageWithQuery(query: query, page: 0) { data, _  in
             guard let volumes = data?.items else { return }
             self.lastPage = 0
             self.volumes = volumes
+            self.fetchNewDataLock = false
         }
     }
 
     func fetchNextPage() {
-        GoogleBooksAPI.getNextPageWithQuery(query: query, page: lastPage) { data, error  in
+        GoogleBooksAPI.getNextPageWithQuery(query: query, page: lastPage) { data, _  in
             guard let volumes = data?.items else { return }
             self.volumes += volumes
             self.nextPageFetchLock = true
@@ -42,11 +57,34 @@ class BooksGridViewModel: NSObject {
     }
 
     func reloadNextPageOnIndexPathThreshold(_ indexPath: IndexPath, threshold: Int) {
-        if indexPath.row >= threshold && nextPageFetchLock {
+        if indexPath.row >= threshold && nextPageFetchLock && showQueryBooks {
             nextPageFetchLock = false
             lastPage += 1
             fetchNextPage()
         }
+    }
+
+    func setUpNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(onBookFavorited(_:)), name: BookCollectionCell.favoriteABookNotificationId, object: nil)
+    }
+
+    @objc
+    func onBookFavorited(_ notification: Notification) {
+        guard let indexPath = notification.userInfo?["indexPath"] as? IndexPath else { return }
+        volumes[indexPath.row].favorited = true
+        favorites.insert(volumes[indexPath.row])
+        guard let onSingleRowReload = onSingleRowReload else { return }
+        onSingleRowReload(indexPath)
+    }
+
+    @objc
+    func toggleViewMode() {
+        showQueryBooks.toggle()
+        if showQueryBooks {
+            searchNewQuery()
+            return
+        }
+        volumes = Array(favorites)
     }
 }
 
@@ -58,9 +96,9 @@ extension BooksGridViewModel: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookCollectionCell.identifier, for: indexPath) as? BookCollectionCell
 
-        cell?.row = indexPath.row
+        volumes[indexPath.row].favorited = favorites.contains(volumes[indexPath.row])
         cell?.viewModel.model = volumes[indexPath.row]
-        reloadNextPageOnIndexPathThreshold(indexPath, threshold: volumes.count - 4)
+        reloadNextPageOnIndexPathThreshold(indexPath, threshold: volumes.count - 1)
 
         return cell ?? UICollectionViewCell()
     }
