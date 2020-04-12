73f4054097e3a098050b4f03d1f1732c9547ac9d
Hugo Flores <hfp.ing2@gmail.com>
2020-04-12 12:17:40 -0400

    UI Tests

---
+++ GoogleBooksUITests/GoogleBooksUITests.swift
@@ -0,0 +1,52 @@
+//
+//  GoogleBooksUITests.swift
+//  GoogleBooksUITests
+//
+//  Created by Hugo Flores Perez on 4/12/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import XCTest
+
+class GoogleBooksUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        let searchBar = app.searchFields["Search for book by title"]
+        
+        let myBooksAppNavigationBar = XCUIApplication().navigationBars["My Books App"]
+        XCTAssertTrue(searchBar.exists)
+        myBooksAppNavigationBar.buttons["Favorites"].tap()
+        XCTAssertFalse(searchBar.exists)
+        myBooksAppNavigationBar.buttons["Search"].tap()
+        XCTAssertTrue(searchBar.exists)
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
